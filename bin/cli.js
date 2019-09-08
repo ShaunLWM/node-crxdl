c62ed459e72b6a6d318d6cef659f2b4af8e35626
ShaunLWM <shaunlimweimin@gmail.com>
2019-09-09 02:01:17 +0800

    Refactoring

--- bin/cli.js
+++ bin/cli.js
@@ -1,21 +1,20 @@
 const download = require("download");
 const path = require("path");
 
-const cws_pattern = /^https?:\/\/chrome.google.com\/webstore\/.+?\/([a-z]{32})(?=[\/#?]|$)/;
-const cws_download_pattern = /^https?:\/\/clients2\.google\.com\/service\/update2\/crx\b.*?%3D([a-z]{32})%26uc/;
+const CWS_PATTERN = /^https?:\/\/chrome.google.com\/webstore\/.+?\/([a-z]{32})(?=[\/#?]|$)/;
+const CWS_DOWNLOAD_PATTERN = /^https?:\/\/clients2\.google\.com\/service\/update2\/crx\b.*?%3D([a-z]{32})%26uc/;
 
 function getExtensionId(url) {
-    let match = cws_pattern.exec(url);
+    let match = CWS_PATTERN.exec(url);
     if (match) return match[1];
-    match = cws_download_pattern.exec(url);
+    match = CWS_DOWNLOAD_PATTERN.exec(url);
     return match && match[1];
 }
-
 
 module.exports = async function ({ url, output = "./" }) {
     let match = getExtensionId(url);
     let extensionId = match ? match : url;
     let crx = `https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=unknown&prodversion=76.0.3809.132&acceptformat=crx2,crx3&x=id%3D${extensionId}%26uc`
-    await download(crx, output, { filename: `${extensionId}.crx`});
+    await download(crx, output, { filename: `${extensionId}.crx` });
     return path.join(output, `${extensionId}.crx`);
 }
