4946729d3b74b41b3a1ccde52f368f2f52035926
ShaunLWM <shaunlimweimin@gmail.com>
2021-03-08 21:41:26 +0800

    Remove dist folder for git

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const getExtensionId: (url: string) => string | null;
-export declare const crxdl: ({ url, output, filename, }: {
-    url: string;
-    output?: string | undefined;
-    filename?: string | undefined;
-}) => Promise<string>;
